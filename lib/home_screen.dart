eb26e2acc462701efcd885ef38656510f536bd06
IvanPartalo <ivanpartalo123@gmail.com>
2024-07-19 22:44:41 +0200

    uploading image as background

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:gallery_saver/gallery_saver.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:painter_app/components/line.dart';
 import 'package:painter_app/menu/pop_up_button.dart';
 import 'package:painter_app/menu/pop_up_color.dart';
@@ -32,6 +35,88 @@ class _HomeScreenState extends State<HomeScreen> {
   List<Line> undoLines = [];
   Color selectedColor = Colors.red;
   double selectedThickness = 1.0;
+  ui.Image? _image;
+  ui.Image? placeHolderImage;
+  bool backgroundChosen = false;
+  bool imageLoaded = false;
+  final picker = ImagePicker();
+
+  @override
+  void initState() {
+    super.initState();
+    loadInitImage();
+  }
+
+  loadInitImage() async{
+    placeHolderImage = await loadPlaceholderImage('assets/logo.png');
+    setState(() {});
+  }
+
+  // za ucitavanje slika pomoc od: https://itchybumr.medium.com/flutter-tutorial-image-picker-picking-photos-from-camera-or-photo-gallery-5243a5eff6b4
+  Future getImageFromGallery() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    setState(() async {
+      if (pickedFile != null) {
+        imageLoaded = false;
+        _image = await loadImageFromFile(File(pickedFile.path));
+        backgroundChosen = true;
+      }
+    });
+  }
+
+  Future getImageFromCamera() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+    setState(() async {
+      if (pickedFile != null) {
+        imageLoaded = false;
+        _image = await loadImageFromFile(File(pickedFile.path));
+        backgroundChosen = true;
+      }
+    });
+  }
+
+  Future<ui.Image> loadImageFromFile(File file) async {
+    Uint8List imageBytes = await file.readAsBytes();
+    Completer<ui.Image> completer = Completer();
+    ui.decodeImageFromList(imageBytes, (ui.Image img) {
+      completer.complete(img);
+    });
+    imageLoaded = true;
+    return completer.future;
+  }
+
+  Future<ui.Image> loadPlaceholderImage(String assetPath) async {
+    final ByteData data = await rootBundle.load(assetPath);
+    final Completer<ui.Image> completer = Completer();
+    ui.decodeImageFromList(Uint8List.view(data.buffer), (ui.Image img) {
+      completer.complete(img);
+    });
+    return completer.future;
+  }
+
+  Future showOptions() async {
+    showCupertinoModalPopup(
+      context: context,
+      builder: (context) => CupertinoActionSheet(
+        actions: [
+          CupertinoActionSheetAction(
+            child: Text('Photo Gallery'),
+            onPressed: () {
+              Navigator.of(context).pop();
+              getImageFromGallery();
+            },
+          ),
+          CupertinoActionSheetAction(
+            child: Text('Camera'),
+            onPressed: () {
+              Navigator.of(context).pop();
+              getImageFromCamera();
+            },
+          ),
+        ],
+      ),
+    );
+  }
 
   void _updateColor(Color color) {
     setState(() {
@@ -47,6 +132,8 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       currentLines = [];
       undoLines = [];
+      backgroundChosen = false;
+      imageLoaded = false;
     });
   }
   void _setLandscapeMode() {
@@ -66,7 +153,7 @@ class _HomeScreenState extends State<HomeScreen> {
     try {
       ui.PictureRecorder pictureRecorder = ui.PictureRecorder();
       Canvas canvas = Canvas(pictureRecorder);
-      DrawingPainter painter = DrawingPainter(currentLines);
+      DrawingPainter painter = DrawingPainter(currentLines, _image!, backgroundChosen, imageLoaded);
       Size? size = context.size;
       //moram da obojim pozadinu u belo rucno :(
       Paint paint = Paint();
@@ -97,6 +184,9 @@ class _HomeScreenState extends State<HomeScreen> {
   }
   @override
   Widget build(BuildContext context) {
+    if(placeHolderImage == null){
+      return const Center(child: CircularProgressIndicator());
+    }
     return Scaffold(
         body: Column(
           children: <Widget>[
@@ -137,8 +227,9 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 IconButton(
                     onPressed: () {
+                      showOptions();
                     },
-                    icon: const Icon(Icons.text_format)
+                    icon: const Icon(Icons.upload_sharp)
                 ),
                 PopUpMore(onDeleteAll: _deleteAll, onSetLandscape: _setLandscapeMode, onSetPortrait: _setPortraitMode,
                 onSaveImage: _saveAsImage,),
@@ -172,7 +263,9 @@ class _HomeScreenState extends State<HomeScreen> {
                       currentLine = null;
                     },
                     child: CustomPaint(
-                      painter: DrawingPainter(currentLines),
+                      painter: _image != null
+                          ? DrawingPainter(currentLines, _image!, backgroundChosen, imageLoaded)
+                          : DrawingPainter(currentLines, placeHolderImage!, backgroundChosen, imageLoaded),
                       child: SizedBox(
                         width: MediaQuery.of(context).size.width,
                         height: MediaQuery.of(context).size.height - 100,
@@ -191,16 +284,39 @@ class _HomeScreenState extends State<HomeScreen> {
 
 class DrawingPainter extends CustomPainter {
   late List<Line> linesToDraw;
-  DrawingPainter(List<Line> lines){
+  late File image;
+  late ui.Image backgroundImage;
+  bool backgroundChosen = false;
+  bool imageLoaded = false;
+  DrawingPainter(List<Line> lines, ui.Image i, this.backgroundChosen, this.imageLoaded){
     if(lines == null){
       linesToDraw = [];
     }else {
       linesToDraw = lines;
     }
+    backgroundImage = i;
+  }
+  DrawingPainter somePainter(List<Line> lines, backgroundChosen, imageLoaded){
+    if(lines == null){
+      linesToDraw = [];
+    }else {
+      linesToDraw = lines;
+    }
+    return this;
   }
   @override
   void paint(Canvas canvas, Size size) {
     final paint = Paint();
+    if(backgroundChosen && imageLoaded){
+      double scale = getScaleFactor(backgroundImage, size);
+      double dx = (size.width - backgroundImage.width.toDouble() * scale ) / 2;
+      double dy = (size.height - backgroundImage.height.toDouble() * scale ) / 2;
+      canvas.save();
+      canvas.translate(dx, dy);
+      canvas.scale(scale, scale);
+      canvas.drawImage(backgroundImage, Offset.zero, paint);
+      canvas.restore();
+    }
     for(var line in linesToDraw){
       paint.color = line.color;
       paint.strokeWidth = line.thickness;
@@ -210,11 +326,18 @@ class DrawingPainter extends CustomPainter {
         canvas.drawLine(currentPoint, nextPoint, paint);
       }
     }
-
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
     return true;
   }
+
+  double getScaleFactor(ui.Image image, Size size){
+    double scaleX = size.width / image.width.toDouble();
+    double scaleY = size.height / image.height.toDouble();
+    double scale = scaleX < scaleY ? scaleX : scaleY;
+    return scale;
+  }
+
 }
